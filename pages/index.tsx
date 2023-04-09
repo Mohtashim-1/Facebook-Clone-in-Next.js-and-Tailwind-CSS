6eb89d81940878cd7dcf7879757691a590bac329
Mohtashim Bandhani <mohtashim.bandhani@saylanitrust.lan>
2023-04-10 00:15:31 +0500

    10-04-2023

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,23 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
+    <div className='bg-gray-200'>
+      <div className="container flex mt-48 mx-auto max-w-full h-96">
+        <div className="left">
+          <img className='w-60' src="facebook.svg" alt="" />
+          <p>Click your picture or add an account.
+          </p>
+        </div>
+        <div className="right flex flex-col">
+          <input type="text" name="email" placeholder='Email address or phone number' id="email" />
+          <input type="password" name='password' id='password' placeholder="Password" />
+          <button className="btn">Log In</button>
+          <span>Forget Password?</span>
+          <p>Create Account</p>
+          <hr />
+        </div>
+      </div>
 
-    </>
+    </div>
   )
 }
